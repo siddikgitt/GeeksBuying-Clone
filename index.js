eb96a8bf6762d7e240186a7ee17d818ec4ef6e66
yedhunanthan <yedhunanthan@gmail.com>
2022-03-31 15:07:15 +0530

    added 3rd and 4th home div

--- index.js
+++ index.js
@@ -1,67 +1,81 @@
+var data=[
+  {name:"dummy data",img:"https://img.gkbcdn.com/p/2021-11-01/Oclean-W1-Oral-Irrigator-White-475349-0._w280_p1_.jpg",print:999,scratchprice:9999,off:"30% OFF"},
+  {name:"dummy data",img:"https://img.gkbcdn.com/p/2021-11-01/Oclean-W1-Oral-Irrigator-White-475349-0._w280_p1_.jpg",print:999,scratchprice:9999,off:"30% OFF"},
+  {name:"dummy data",img:"https://img.gkbcdn.com/p/2021-11-01/Oclean-W1-Oral-Irrigator-White-475349-0._w280_p1_.jpg",print:999,scratchprice:9999,off:"30% OFF"},
+  {name:"dummy data",img:"https://img.gkbcdn.com/p/2021-11-01/Oclean-W1-Oral-Irrigator-White-475349-0._w280_p1_.jpg",print:999,scratchprice:9999,off:"30% OFF"},
+  {name:"dummy data",img:"https://img.gkbcdn.com/p/2021-11-01/Oclean-W1-Oral-Irrigator-White-475349-0._w280_p1_.jpg",print:999,scratchprice:9999,off:"30% OFF"},
+  {name:"dummy data",img:"https://img.gkbcdn.com/p/2021-11-01/Oclean-W1-Oral-Irrigator-White-475349-0._w280_p1_.jpg",print:999,scratchprice:9999,off:"30% OFF"},
+  {name:"dummy data",img:"https://img.gkbcdn.com/p/2021-11-01/Oclean-W1-Oral-Irrigator-White-475349-0._w280_p1_.jpg",print:999,scratchprice:9999,off:"30% OFF"},
+  {name:"dummy data",img:"https://img.gkbcdn.com/p/2021-11-01/Oclean-W1-Oral-Irrigator-White-475349-0._w280_p1_.jpg",print:999,scratchprice:9999,off:"30% OFF"},
+  {name:"dummy data",img:"https://img.gkbcdn.com/p/2021-11-01/Oclean-W1-Oral-Irrigator-White-475349-0._w280_p1_.jpg",print:999,scratchprice:9999,off:"30% OFF"},
+  {name:"dummy data",img:"https://img.gkbcdn.com/p/2021-11-01/Oclean-W1-Oral-Irrigator-White-475349-0._w280_p1_.jpg",print:999,scratchprice:9999,off:"30% OFF"},
+]
 var slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
-  showDivs(slideIndex += n);
+showDivs(slideIndex += n);
 }
 
 function currentDiv(n) {
-  showDivs(slideIndex = n);
+showDivs(slideIndex = n);
 }
 
 function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-white", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
+var i;
+var x = document.getElementsByClassName("mySlides");
+var dots = document.getElementsByClassName("demo");
+if (n > x.length) {slideIndex = 1}
+if (n < 1) {slideIndex = x.length}
+for (i = 0; i < x.length; i++) {
+  x[i].style.display = "none";  
 }
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+for (i = 0; i < dots.length; i++) {
+  dots[i].className = dots[i].className.replace(" w3-white", "");
 }
-
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-white", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
+x[slideIndex-1].style.display = "block";  
+dots[slideIndex-1].className += " w3-white";
 }
 
 var myIndex = 0;
 carousel();
 
 function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 4000);
+var i;
+var x = document.getElementsByClassName("mySlides");
+for (i = 0; i < x.length; i++) {
+  x[i].style.display = "none";  
 }
+myIndex++;
+if (myIndex > x.length) {myIndex = 1}    
+x[myIndex-1].style.display = "block";  
+setTimeout(carousel, 4000);
+}
+// Set the date we're counting down to
+var countDownDate = new Date("apr 5, 2022 15:37:25").getTime();
+
+
+var x = setInterval(function() {
+
+var now = new Date().getTime();
+  
+var distance = countDownDate - now;
+  
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  if(days<10){days= "0"+days}
+  if(hours<10){hours= "0"+hours}
+  if(minutes<10){minutes= "0"+minutes}
+  if(seconds<10){seconds= "0"+seconds}
+document.querySelector(".day").innerHTML = days;
+document.querySelector(".hur").innerHTML = hours;
+document.querySelector(".min").innerHTML = minutes;
+document.querySelector(".sec").innerHTML = seconds;
+  
+if (distance < 0) {
+  clearInterval(x);
+  document.querySelector("h4").innerText = "EXPIRED";
+}
+}, 1000);
